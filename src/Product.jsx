ed627bd27b4c9c8d91012b1ce4e6b6c1ebde3cbc
sohel khan <sohelkhan060404@gmail.com>
2026-01-05 19:21:00 +0530

    add description to product

--- src/Product.jsx
+++ src/Product.jsx
@@ -4,10 +4,11 @@ import Price from './Price';
 function Product ({title, idx}){
     let oldPrices = ["12,500","11,900","1,599","599"];
     let newPrices = ["8,999","10,000","1,399","399"];
+    let Description = ["8,000 DPI","intuitive surface","Designed for iPad pro","wireless"];
     return (
         <div className='product' >
         <h4>{title}</h4>
-        <p>Description</p>
+        <p>{Description[idx]}</p>
         <Price oldPrice={oldPrices[idx]} newPrice={newPrices[idx]}/>
 
         </div>
