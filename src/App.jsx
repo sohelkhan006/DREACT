4469e520f77bf569b45a7eb4562ac167c390d453
sohel khan <sohelkhan060404@gmail.com>
2026-01-05 19:10:30 +0530

    Remove unwanted imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,16 @@
-// import React from 'react';
+
 import './App.css'
-import Title from './Title';
 import ProductTab from './ProductTab';
-import Greeting from './Greeting';
-import Form from './Form';
-import HelloUser from './Activity';
+
+
+
 
 function App() {
   return (
     <div>
-      <HelloUser username= "sohel" textcolor= "red"/>
+      
       <ProductTab/>
-      {/* <h1>Server is running successfully</h1>
-      <Greeting/>
-      <Form/> */}
+    
      </div>
   );
 }
