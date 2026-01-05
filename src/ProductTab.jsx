7a8dceb489fda54f497ca140d3633d6ca2a9e7a5
sohel khan <sohelkhan060404@gmail.com>
2026-01-05 19:25:11 +0530

    add styles to productTab

--- src/ProductTab.jsx
+++ src/ProductTab.jsx
@@ -2,14 +2,20 @@ import './product.css'
 import Product from './Product'
 
 function ProductTab (){
+    let styles = {
+        display : "flex",
+        flexWrap : "wrap",
+        justfyContent : "center",
+        alignItems : "center"
+    }
    
     return (
-        <>
+        <div style={styles}>
         <Product title="LogitechmX Master" idx={0} />
         <Product title="Apple pencil (2nd gen)" idx={1}/>
         <Product title="Zebronics Zeb- Transformer" idx={2}/>
         <Product title="Portonics toad 23" idx={3}/>
-        </>
+        </div>
     );
 }
 
